b964b4d2948065a191c22ecd9af14a0251a7df44
soultomind <soultomind@naver.com>
2020-04-01 22:42:44 +0900

    프로젝트 생성

--- WFAAgent/MainForm.Designer.cs
+++ WFAAgent/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WFAAgent
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 필수 디자이너 변수입니다.
--- WFAAgent/Program.cs
+++ WFAAgent/Program.cs
@@ -15,7 +15,7 @@ namespace WFAAgent
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
