96f636b432c76962f338645c167ee8353015e16d
soultomind <soultomind@naver.com>
2020-04-10 22:41:22 +0900

    단순 - 소켓 이벤트핸들러 이벤트 인자 작업 중

--- WFAAgent.Framework/Net/Sockets/SocketEventArgs.cs
+++ WFAAgent.Framework/Net/Sockets/SocketEventArgs.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,70 @@ namespace WFAAgent.Framework.Net.Sockets
 {
     public abstract class SocketEventArgs : EventArgs
     {
+        public SocketEventArgs(Socket socket)
+        {
+            EventSocket = socket;
+        }
 
+        protected Socket EventSocket
+        {
+            get;
+            private set;
+        }
+    }
+
+    public class ServerSocketEventArgs : SocketEventArgs
+    {
+        public ServerSocketEventArgs(Socket socket) : base(socket)
+        {
+        }
+    }
+    public class ServerListenEventArgs : ServerSocketEventArgs
+    {
+        public ServerListenEventArgs(Socket socket) : base(socket)
+        {
+
+        }
+
+        public Socket ServerSocket
+        {
+            get { return EventSocket; }
+        }
+    }
+
+    public class ServerClientAcceptEventArgs : ServerSocketEventArgs
+    {
+        public ServerClientAcceptEventArgs(Socket socket) : base(socket)
+        {
+        }
+
+        public Socket ServerSocket
+        {
+            get { return EventSocket; }
+        }
+
+        public Socket ClientSocket
+        {
+            get;
+            set;
+        }
+    }
+
+    public class ServerClientDisconnectEventArgs : ServerSocketEventArgs
+    {
+        public ServerClientDisconnectEventArgs(Socket socket) : base(socket)
+        {
+        }
+
+        public Socket ServerSocket
+        {
+            get { return EventSocket; }
+        }
+
+        public Socket ClientSocket
+        {
+            get;
+            set;
+        }
     }
 }
