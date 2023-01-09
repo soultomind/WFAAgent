f80792133931625cf4c91375d1fd16ec41c54a9b
soultomind <soultomind930@gmail.com>
2023-01-09 22:41:30 +0900

    단순 - 서버폼으로 Out,Error 데이터 전송 메서드 추가

--- WFAAgent/ServerForm.Core.cs
+++ WFAAgent/ServerForm.Core.cs
@@ -8,5 +8,20 @@ namespace WFAAgent
 {
     partial class ServerForm
     {
+        internal void ServerForm_SendMonitoringOutputData(string data)
+        {
+            if (Console.IsOutputRedirected)
+            {
+                Console.Out.WriteLine(data);
+            }
+        }
+
+        internal void ServerForm_SendMonitoringErrorData(string data)
+        {
+            if (Console.IsErrorRedirected)
+            {
+                Console.Error.WriteLine(data);
+            }
+        }
     }
 }
